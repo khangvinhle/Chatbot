e0e6c3609bb89aac5cfb4390331e2a164404d6a2
Le Vinh Khang <1559019@itec.hcmus.edu.vn>
2018-06-21 00:31:52 +0700

    sua file app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ appPassword: APPLICATION_PASSWORD
 });
 
 // Listen for messages from users
-server.post('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/22e76c57-0276-42cf-a60a-9b310ff24930?subscription-key=c25e399bd6ca4e9890cb42fd829e70d6&verbose=true&timezoneOffset=420&q=', connector.listen());
+server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back
 (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
