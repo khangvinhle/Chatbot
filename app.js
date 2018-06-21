4c7528947e8443d166d1c7835ebb813043eb28de
Le Vinh Khang <1559019@itec.hcmus.edu.vn>
2018-06-21 11:18:00 +0700

    update file app.js

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
+
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 var APPLICATION_ID = 'b57e77ea-0141-48be-b8c1-003d442a11d1';
 var APPLICATION_PASSWORD = 'ffwKKW3503-nqagTQWC7-*[';
 var connector = new builder.ChatConnector({
-appId: APPLICATION_ID,
-appPassword: APPLICATION_PASSWORD
+    appId: APPLICATION_ID,
+    appPassword: APPLICATION_PASSWORD
 });
+
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
+
+LUIS_APPLICATION_ID = '22e76c57-0276-42cf-a60a-9b310ff24930';
+LUIS_SUBSCRIPTION_KEY = 'aa5263f3be8543adbcbba858f4483a89';
+LUIS_URL = 'https://southeastasia.api.cognitive.microsoft.com/luis/v2.0/apps/' + LUIS_APPLICATION_ID;
+
+function getIntentFromLuis(text, callback) {
+    request.get({
+        url: LUIS_URL,
+        qs: {
+            'subscription-key': LUIS_SUBSCRIPTION_KEY,
+            'timezoneOffset': 0,
+            'verbose': true,
+            'q': text
+        },
+        json: true
+    }, function(error, response, data) {
+        if (error) {
+            callback(error);
+        } else {
+            callback(null, data);
+        }
+    });
+}
+
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-session.send("You said: %s", session.message.text);
+var bot = new builder.UniversalBot(connector, function(session) {
+    getIntentFromLuis(session.message.text, function(error, luisData) {
+        var intent = luisData.topScoringIntent.intent;
+        var score = luisData.topScoringIntent.score;
+        if (score > 0.6 && intent != 'None') {
+            if (intent == 'product lookup') {
+                session.send("Sure, I will show you all the products!");
+            } else if (intent == 'location lookup') {
+                session.send("Sorry, we are selling online only.");
+            }
+        } else {
+            session.send("I did not understand you. I am still learning! Can you rephrase?");
+        }
+    });
 });
